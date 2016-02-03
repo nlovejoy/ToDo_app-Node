efec3fa841cf4a74b11514a9f5383a73765c1b7f
nflovejoy <nflovejoy@gmail.com>
2016-02-03 23:59:06 +0000

    added session handling

--- index.js
+++ index.js
@@ -3,18 +3,41 @@ var exphbs  = require('express-handlebars');
 var app = express();
 var bodyParser = require('body-parser');
 var Users = require('./models/users.js')
+var session = require('express-session')
 
 //configure our app
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(session({ //   .use is express middleware, read about this
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: 'auto' }
+}))
+
+app.use(function(req, res, next){
+  if(req.session.userId){
+    Users.findById(req.session.userId, function(err, user){
+      if(!err){
+        res.locals.currentUser = user;
+      }
+      next();
+    })
+  }else{
+    next();  
+  }
+})
 
 app.get('/', function (req, res) {
   Users.count(function (err, users) {
     if (err) {
       res.send('error getting users');
     }else{
-      res.render('index', {userCount: users.length});
+      res.render('index', {
+        userCount: users.length,
+        currentUser: res.locals.currentUser
+      });
     }
   });
 });
@@ -23,12 +46,12 @@ app.post('/user/register', function (req, res) {
     if(req.body.password !== req.body.password_confirmation){
         return res.render('index', {errors: "Password and password confirmation dont match"});
     }  
-  
     var newUser = new Users();
     newUser.hashed_password = req.body.password;
     newUser.email = req.body.email;
     newUser.name = req.body.fl_name;
-    newUser.save(function(err){
+    newUser.save(function(err, user){
+      req.session.userId = user._id;
       if(err){
         res.render('index', {errors: err});
       }else{
