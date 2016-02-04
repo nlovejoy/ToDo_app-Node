ba774aa55686ad6a0b4a13afd67c30fbb7054ba9
nflovejoy <nflovejoy@gmail.com>
2016-02-04 23:47:27 +0000

    added persistance and hashing

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ var exphbs  = require('express-handlebars');
 var app = express();
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var MongoDBStore = require('connect-mongodb-session')(session);
 var Users = require('./models/users.js');
 
-
 //configure our app
+var store = new MongoDBStore({ 
+  uri: process.env.MONGO_URL,
+  collection: 'sessions'
+});
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
@@ -14,8 +18,10 @@ app.use(session({ //   .use is express middleware, read about this
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: 'auto' }
+  cookie: { secure: 'auto' },
+  store: store
 }))
+
 
 app.use(function(req, res, next){
   console.log('req.session =', req.session);
@@ -30,6 +36,8 @@ app.use(function(req, res, next){
     next();  
   }
 })
+
+
 
 app.get('/', function (req, res) {
   Users.count(function (err, users) {
@@ -48,19 +56,44 @@ app.post('/user/register', function (req, res) {
     if(req.body.password !== req.body.password_confirmation){
         return res.render('index', {errors: "Password and password confirmation dont match"});
     }  
+    
+    
     var newUser = new Users();
     newUser.hashed_password = req.body.password;
     newUser.email = req.body.email;
     newUser.name = req.body.fl_name;
     newUser.save(function(err, user){
-      req.session.userId = user._id;
       if(err){
+        err = 'Error registering you!';
         res.render('index', {errors: err});
       }else{
+        req.session.userId = user._id;
         res.redirect('/');
       }
     })
     console.log('The user has email adddress ' + req.body.email);
+});
+
+
+app.post('/user/login', function (req, res) {
+  var user = Users.findOne({email: req.body.email}, function(err, user){
+    if(err || !user){
+      res.send('Bad login, no such user');
+      return;
+    }
+    console.log('user =', user);
+    console.log('actual password =', user.hashed_password);
+    console.log('provided password =', req.body.password);
+    
+    user.comparePassword(req.body.password, function(err, isMatch){
+      if(err || !isMatch){
+        res.send('bad password meng');
+      }else{
+        req.session.userId = user._id;
+        res.redirect('/')          
+      }
+    });
+  })
 });
 
 app.get('/user/logout', function(req, res){
